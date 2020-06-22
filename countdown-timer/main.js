7a56ff4c64def9cd2a0318b1316aac248318cd7c
Kim Minh <kimminh.tuts@gmail.com>
2020-06-22 14:50:01 +0700

    Countdown timer

---
+++ countdown-timer/main.js
@@ -0,0 +1,28 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 31, 2020 23:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;  
+  
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  document.getElementById("s1").innerHTML = days;
+  document.getElementById("s2").innerHTML = hours;
+  document.getElementById("s3").innerHTML = minutes;
+  document.getElementById("s4").innerHTML = seconds;
+  
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
